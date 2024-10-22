8461052087cf03abc944e4a6b82bc6723fd09a05
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2024-10-22 23:49:08 +0530

    complate TODO list with react context

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,67 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/about.jsx";
-import Header from "./components/Header/Header.jsx";
-import User from "./components/User.jsx";
-import UserContextProvider from "./context/UserContextProvider.jsx";
+import { useEffect, useState } from "react";
+import { TodoProvider } from "./contexts";
+import { TodoForm, TodoList } from "./components";
+
 const App = () => {
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = (todo) => {
+		setTodos((pvs) => [{ id: Date.now(), ...todo }, ...pvs]);
+	};
+
+	const updateTodo = (id, todo) => {
+		setTodos((pvs) => pvs.map((t) => (t.id === id ? todo : t)));
+	};
+
+	const removeTodo = (id) => {
+		setTodos((pvs) => pvs.filter((t) => t.id !== id));
+	};
+
+	const toggleComplete = (id) => {
+		setTodos((pvs) =>
+			pvs.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
+		);
+	};
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem("todos"));
+		if (data && data.length > 0) {
+			setTodos(data);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	}, [todos]);
+
 	return (
 		<>
-			<UserContextProvider>
-				<BrowserRouter>
-					<Header />
-					<Routes>
-						{" "}
-						<Route
-							path="/home"
-							element={<Home />}
-						/>
-						<Route
-							path="/home/new"
-							element={
-								<div className="bg-rose-200 text-5xl">This is /Home/new</div>
-							}
-						/>
-						<Route
-							path="/about"
-							element={<About />}
-						/>
-						<Route
-							path="/login"
-							element={<User />}
-						/>
-					</Routes>
-				</BrowserRouter>
-			</UserContextProvider>
+			<TodoProvider
+				value={{ todos, addTodo, updateTodo, removeTodo, toggleComplete }}
+			>
+				<div className="bg-[#172842] min-h-screen py-8">
+					<div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+						<h1 className="text-2xl font-bold text-center mb-8 mt-2">
+							Manage Your Todos
+						</h1>
+						<div className="mb-4">
+							{/* Todo form goes here */}
+							<TodoForm />
+						</div>
+						<div className="flex flex-wrap gap-y-3">
+							{/*Loop and Add TodoItem here */}
+							{todos.map((todo) => (
+								<div
+									key={todo.id}
+									className="w-full"
+								>
+									<TodoList todo={todo} />
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+			</TodoProvider>
 		</>
 	);
 };
--- src/components/Footer/Footer.jsx
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-const Footer = () => {
-  return <div className="bg-gray-400 text-4xl">This is Footer</div>;
-};
-
-export default Footer;
